aa20a37bcbd1c4290cb7005b6ba7ca5cbc2006cc
Hemanth Kumar sarisa <119401340+Hemanth252k3@users.noreply.github.com>
2025-06-10 13:50:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // --- Product Data ---
     const products = [
         // All product image paths now correctly start with 'Retail_Website/'
-        { id: 1, name: 'Movado series 800', price: 199.99, image: './Mens_Watch/Movado_series_800.jfif', category: 'mens_watch' },
+        { id: 1, name: 'Movado series 800', price: 199.99, image: '/Mens_Watch/Movado_series_800.jfif', category: 'mens_watch' },
         { id: 2, name: 'ALexandre Christie', price: 99.50, image: './Womens_Watch/ALexandre_Christie.jfif', category: 'womens_watch' },
         { id: 3, name: 'Daniel Klein', price: 149.00, image: './Womens_Watch/Daniel_Klein_Women_watch.jpg', category: 'womens_watch' },
         { id: 4, name: 'Luminox master', price: 75.00, image: './Mens_Watch/Luminox_master.jfif', category: 'mens_watch' },
